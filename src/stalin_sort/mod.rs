4601cbbe3a3d78b85ce477500a5581e9530a413a
Nikolai Vazquez <nikvzqz@gmail.com>
2019-10-24 14:38:16 -0400

    Add to StalinSort docs

--- src/stalin_sort/mod.rs
+++ src/stalin_sort/mod.rs
@@ -3,6 +3,14 @@ mod vec;
 /// A single pass, no-nonsense sorting algorithm with O(n) complexity that
 /// removes elements until the value is sorted.
 ///
+/// This is a reference to [Joseph Stalin]'s horrible regime over Russia.
+///
+/// > Official records reveal 799,455 documented executions in the Soviet Union
+/// > between 1921 and 1953; 681,692 of these were carried out between 1937 and
+/// > 1938, the years of the [Great Purge].
+///
+/// # Source
+///
 /// <img src="https://i.redd.it/x9triplll1v11.jpg" width="360px">
 ///
 /// <q>I came up with a single pass O(n) sort algorithm I call StalinSort. You
@@ -10,6 +18,9 @@ mod vec;
 /// which is out of order is eliminated. At the end you have a sorted list.</q>
 /// <br>
 /// <i>- mathew ✅ (@mathew@mastodon.social)</i>
+///
+/// [Joseph Stalin]: https://en.wikipedia.org/wiki/Joseph_Stalin
+/// [Great Purge]:   https://en.wikipedia.org/wiki/Great_Purge
 pub trait StalinSort {
     /// Removes unsorted elements until `self` is sorted.
     ///
