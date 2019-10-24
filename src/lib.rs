a95a4fe30526a1086efb2b9381226e3c8d81cebe
Nikolai Vazquez <nikvzqz@gmail.com>
2019-10-24 17:41:35 -0400

    Add crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,35 @@
-//! Unlicensed bad ideas.
+//! A collection of (bad) ideas in Rust that you may or may not want to make use of
+//! in your next big project. Courtesy of
+//! [Nikolai Vazquez](https://twitter.com/NikolaiVazquez).
+//!
+//! ## Installation
+//!
+//! This crate is available [on crates.io](https://crates.io/crates/bad) and can be
+//! used by adding the following to your project's
+//! [`Cargo.toml`](https://doc.rust-lang.org/cargo/reference/manifest.html):
+//!
+//! ```toml
+//! [dependencies]
+//! bad = "0.0.0"
+//! ```
+//!
+//! and optionally add this to your crate root (`main.rs` or `lib.rs`):
+//!
+//! ```rust
+//! extern crate bad;
+//! ```
+//!
+//! ## License
+//!
+//! This project is released under either:
+//!
+//! - [The Unlicense](https://github.com/nvzqz/bad-rs/blob/master/UNLICENSE)
+//!
+//! - [MIT License](https://github.com/nvzqz/bad-rs/blob/master/LICENSE-MIT)
+//!
+//! at your choosing.
+//!
+//! [`StalinSort`]: trait.StalinSort.html
 
 #![deny(missing_docs)]
 
